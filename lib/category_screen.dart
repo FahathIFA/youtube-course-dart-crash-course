a5d7ca65779b057f41cf22377d2a7b504c586cbc
iliyasifa <iliyasifa@gmail.com>
2023-02-12 10:27:04 +0530

    meals app first commit

---
+++ lib/category_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app_flutter/category_item.dart';
+import 'package:meals_app_flutter/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategories
+            .map(
+              (catData) => CategoryItem(
+                title: catData.title,
+                color: catData.color,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app_flutter/category_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.pink,
+        ).copyWith(
+          secondary: Colors.amber,
+        ),
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
